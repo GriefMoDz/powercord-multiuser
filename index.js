d28d3360506ef6b22bda3fc7ca515791d4622728
GriefMoDz <GriefMoDz@gmail.com>
2019-07-19 21:20:12 +1000

    [v0.1.2] New injection method and other fixes

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const { remote } = require('electron');
-const { remote: { BrowserWindow } } = require('electron');
-const { resolve } = require('path');
-
 const { Plugin } = require('powercord/entities');
-const { contextMenu, getModule, getModuleByDisplayName, React } = require('powercord/webpack');
+const { React, getModule, contextMenu } = require('powercord/webpack');
 const { forceUpdateElement, getOwnerInstance, waitFor } = require('powercord/util');
 const { inject, uninject } = require('powercord/injector');
 const { ContextMenu } = require('powercord/components');
@@ -11,18 +7,27 @@ const { ContextMenu } = require('powercord/components');
 const Settings = require('./core/components/Settings');
 
 module.exports = class Mu extends Plugin {
-  startPlugin () {
-    this.loadCSS(resolve(__dirname, 'core/styles/style.scss'));
-    this.injectContextMenu();
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      avatarQuery: ''
+    }
+  }
+
+  async startPlugin () {
+    this.loadCSS(require('path').resolve(__dirname, 'core/styles/style.scss'));
     this.registerSettings('mu', 'Mu', (props) => React.createElement(Settings, {
       ...props,
       saveHandler: this.handleSave.bind(this)
     }));
+
+    this.injectContextMenu();
   }
 
   pluginWillUnload () {
     uninject('pc-mu-avatar');
-    forceUpdateElement('.pc-inner');
+    forceUpdateElement(this.state.avatarQuery);
   }
 
   handleSave () {
@@ -31,37 +36,52 @@ module.exports = class Mu extends Plugin {
 
   async injectContextMenu () {
     const currentUserId = (await getModule([ 'getId' ])).getId();
-    const Avatar = (await getModuleByDisplayName('FluxContainer(Avatar)'));
 
-    inject('pc-mu-avatar', Avatar.prototype, 'render', (_, res) => {
-      if (res.props.user.id === currentUserId && res.props.size === 'small') {
-        res.props.onContextMenu = (e) => {
-          const { pageX, pageY } = e;
-          const users = this.settings.get('users', []);
+    const avatarClasses = (await getModule([ 'container', 'usernameContainer' ]));
+    const avatarQuery = `.${avatarClasses.avatar.replace(/ /g, '.')}`;
 
-          contextMenu.openContextMenu(e, () =>
-            React.createElement(ContextMenu, {
-              pageX,
-              pageY,
-              itemGroups: [
-                users.map(u => ({
-                  type: 'button',
-                  name: `Open ${u.nickname || 'Untitled'}`,
-                  onClick: () => this.createNewInstance(u.token)
-                }))
-              ]
-            })
-          );
-        };
+    const instance = getOwnerInstance(await waitFor(avatarQuery));
+    inject('pc-mu-avatar', instance.__proto__, 'render', (_, res) => {
+      if (res.props && res.props.children && res.props.children.props) {
+        const avatar = res.props.children.props;
+        const avatarUserId = avatar.src ? (new URL(avatar.src).pathname).split('/')[2] : null;
+
+        if (avatarUserId === currentUserId && avatar.size === 'SIZE_32') {
+          res.props.onContextMenu = (e) => {
+            const { pageX, pageY } = e;
+            const users = this.settings.get('users', []);
+
+            contextMenu.openContextMenu(e, () =>
+              React.createElement(ContextMenu, {
+                pageX,
+                pageY,
+                itemGroups: [
+                  users.map(u => ({
+                    type: 'button',
+                    name: `Open ${u.nickname || 'Untitled'}`,
+                    onClick: () => this.createNewInstance(u.token)
+                  }))
+                ]
+              })
+            );
+          };
+        }
       }
 
       return res;
     });
 
-    getOwnerInstance(await waitFor('.pc-inner')).forceUpdate();
+    this.state.avatarQuery = avatarQuery;
+
+    if (this.state.avatarQuery) {
+      forceUpdateElement(this.state.avatarQuery);
+    }
   }
 
   createNewInstance (token) {
+    const { remote } = require('electron');
+    const { remote: { BrowserWindow } } = require('electron');
+
     const route = `https:${GLOBAL_ENV.WEBAPP_ENDPOINT}/channels/@me`;
     const opts = {
       ...BrowserWindow.getFocusedWindow().webContents.browserWindowOptions,
